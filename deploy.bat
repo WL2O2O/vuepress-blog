5ef33408088fcb66bbe4fcd76e7da585f482eb7a
WLei224 <1149630057@qq.com>
2023-05-30 01:32:31 +0800

    update article

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 @echo off
 git add .
 git commit -m "update article"
-git pull origin master
-git push origin master
+@REM git pull origin master
+@REM git push origin master
 git pull github master
 git push github master
 
